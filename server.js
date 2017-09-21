75513363b5a81452186336dd4d930a9f1521c78d
fmaldonados <felixmaldonado@unitec.edu>
2017-09-21 17:08:23 -0600

    conexion a backend

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ server.connection({
     routes: {cors: true}
 });
 
-//mongoose.connect('mongodb://127.0.0.1:27017/PE2');
-mongoose.connect('mongodb://admin:admin@ds141534.mlab.com:41534/pets-home');
+mongoose.connect('mongodb://admin:admin@ds147044.mlab.com:47044/examen2');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function callback() {
